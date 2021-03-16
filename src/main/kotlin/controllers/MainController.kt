b8b62f8189908d4b99ed70d4c3382b5ec3339854
Denis Korolev <denikorol19999@mail.ru>
2021-03-16 18:12:25 +0300

    Initial commit

---
+++ src/main/kotlin/controllers/MainController.kt
@@ -0,0 +1,13 @@
+package controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+
+    @GetMapping("/")
+    fun helloKotlin(): String {
+        return "hello world"
+    }
+}
